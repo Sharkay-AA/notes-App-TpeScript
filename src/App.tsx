63ff216f66a3585f33012f5c35f91d11ce1c0a6c
Jason Lavigne <jasonlavigne824@gmail.com>
2022-03-23 14:18:49 +0100

    Push to Gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Header from './Components/Header/Header';
+import CreateNotes from './Components/Notes/CreateNotes';
+import NotesList from './Components/Notes/NotesList';
+import {Note} from "./modelsTS/notesModel";
+
 
 function App() {
+
+  const [notes,setNotes] = useState <Note[]>([{
+    id: (new Date).toString(),
+    title: "Bienvenue",
+    text: "Voici un simple projet pour mettre en pratique mes connaissances en react/typescript",
+    color : "rgb(166, 211, 250)",
+    date: (new Date).toString()
+  }]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+          <NotesList notes={notes} setNotes={setNotes}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CreateNotes notes={notes} setNotes={setNotes}/>
+        </Col>
+      </Row>
+    </Container>
+    </>
   );
 }
 
